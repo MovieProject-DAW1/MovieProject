a883e27e9f59188930b5686838849431800a7676
Tanausu suarez <tanausu.suarez101@alu.ulpgc.es>
2018-11-10 15:14:32 +0000

    step 4: added sección login and control navegation

--- nav/nav.js
+++ nav/nav.js
@@ -22,8 +22,12 @@ function loginValidation() {
         for (i = 0; i < json_data.length; i++) {
 
             if (json_data[i].email === email ) {
-                if (password === json_data[i].password) show_alert( 'alert-success' , 'Bienvenido a Moview DAW' );
-                else show_alert( 'alert-danger' , 'ERROR: contraseña incorrecta' );
+                if (password === json_data[i].password) {
+                    showAlert( 'alert-success' , 'Bienvenido a Moview DAW' );
+                    setCookie( 'username', email, 1 );
+                    buildNavegation();
+                }
+                else showAlert( 'alert-danger' , 'ERROR: contraseña incorrecta' );
                 return false;
             }
 
@@ -32,30 +36,34 @@ function loginValidation() {
         /* datos from LOCALSTORE */
         if (localStorage.getItem(email)){
             var user_localstore =  JSON.parse(localStorage.getItem(email));
-            if (password === user_localstore.password) show_alert( 'alert-success' , 'Bienvenido a Moview DAW' );
-            else show_alert( 'alert-danger' , 'ERROR: contraseña incorrecta' );
+            if (password === user_localstore.password) {
+                showAlert( 'alert-success' , 'Bienvenido a Moview DAW' );
+                setCookie( 'username', email, 1 );
+                buildNavegation();
+            }
+            else showAlert( 'alert-danger' , 'ERROR: contraseña incorrecta' );
             return false;
 
         }
-        show_alert( 'alert-warning' , 'Warning: Este usuario no está registrado' );
+        showAlert( 'alert-warning' , 'Warning: Este usuario no está registrado' );
 
     };
     return false;
 }
-function show_alert(add_class, str) {
+function showAlert(add_class, str) {
 
     /* destroy alert if exist */
     var NodeChild = document.getElementById('alert-login');
     if ( NodeChild != null ) NodeChild.parentNode.removeChild(NodeChild);
 
-    build_alert();
+    buildAlert();
 
     var id_text_alert = document.getElementById( 'message-alert' );
     $( "#alert-login" ).addClass(add_class);
     id_text_alert.appendChild(document.createTextNode(str));
     $( '#open-login' ).modal( 'hide' );
 }
-function build_alert() {
+function buildAlert() {
     var elem_div = document.createElement( 'div' );
     elem_div.setAttribute( 'id' , 'alert-login' );
     elem_div.setAttribute( 'class' , 'alert show' );
@@ -74,37 +82,32 @@ function build_alert() {
 function fuction_noFound() {
     window.alert("Lo sentimos, esta función aun no está implementada");
 }
+
+
+
+
+/************************** COOKIES *************************************/
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
-    var cadena = cname + "=" + cvalue + ";" + expires + ";path=/";
-    document.cookie = cadena;
+    var chain = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = chain;
 }
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
     for(var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+        while (c.charAt(0) == ' ')    c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
     }
     return "";
 }
-function checkCookie() {
-    var user=getCookie("username");
-    if (user !== "") {
-        alert("Bienvenido " + user);
-    }
-}
 
 
 
-/************************** EVENTOS  *************************************/
+/************************** EVENTS  *************************************/
 
 
 var link = {
@@ -114,19 +117,18 @@ var link = {
     footer : 'http://localhost:63342/MovieProject/footer/footer.html'
 };
 
-function clearClassActive(id) {
-    $( "#link-index" ).removeClass( 'active' );
-    $( "#link-news" ).removeClass( 'active' );
-    $( "#link-login" ).removeClass( 'active' );
-    $( id ).addClass( 'active' );
-}
+var buttonNavegation = [
+    {
+        name : "Home",
+        id : "link-index"
+    },
+    {
+        name : "Noticias",
+        id : "link-news"
+    }
+];
 
-$( document ).ready( function () {
-    clearClassActive( document.getElementById( 'link-index' ) );
-    $( "#content-body" ).load( link.index );
-    $( "#content-footer" ).load( link.footer );
-
-
+function buttonNavAction() {
     $( "#link-index" ).click(function () {
 
         $( "#content-body" ).load( link.index );
@@ -142,6 +144,82 @@ $( document ).ready( function () {
         $( '#open-login' ).modal( 'hide' );
         clearClassActive( document.getElementById( 'link-login' ) );
     });
+    $( "#link-logout" ).click(function () {
+        console.log( 'logout' );
+        setCookie( 'username', '', 0);
+        buildNavegation();
+        showAlert( 'alert-success' , 'Ha salido satisfactoriamente, ¡hasta pronto!' );
+    });
+
+
+}
+function clearClassActive(id) {
+    $( "#link-index" ).removeClass( 'active' );
+    $( "#link-news" ).removeClass( 'active' );
+    $( "#link-login" ).removeClass( 'active' );
+    $( id ).addClass( 'active' );
+}
+function loadDefault() {
+    clearClassActive( document.getElementById( 'link-index' ) );
+    $( "#content-body" ).load( link.index );
+    $( "#content-footer" ).load( link.footer );
+}
+function buildNavegation() {
+
+
+    var navHTML = '<ul class="navbar-nav mr-auto ">';
+    for (var i = 0; i < buttonNavegation.length; i++) {
+        navHTML += '<li class="nav-item ">\n' +
+            '           <a  id="' + buttonNavegation[i].id + '" class="nav-link" href="#">' + buttonNavegation[i].name + ' </a>\n' +
+            '       </li>';
+    }
+
+
+    var user = getCookie( 'username' );
+    console.log("Usuario logeado-> "+user);
+
+
+    if (user == '') {
+        navHTML += '<li class="nav-item ">\n' +
+            '           <a id="link-login" class="nav-link" href="#open-login" data-toggle="modal">Entrar</a>\n' +
+            '       </li>';
+        navHTML += '</ul>';
+    } else {
+        navHTML += '</ul>';
+        navHTML +='<ul class="navbar-nav justify-content-end">';
+        navHTML +='<li class="nav-item dropdown">\n' +
+            '        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\n' +
+            '          '+getCookie('username') +'\n' +
+            '        </a>\n' +
+            '        <div class="dropdown-menu" aria-labelledby="navbarDropdown">\n' +
+            '          <a class="dropdown-item" href="#">Mi cuenta</a>\n' +
+            '          <a class="dropdown-item" href="#">Mis películas</a>\n' +
+            '          <div class="dropdown-divider"></div>\n' +
+            '          <a id="link-logout" class="dropdown-item" href="#">Salir</a>\n' +
+            '        </div>\n' +
+            '      </li>';
+        navHTML += '</ul>';
+    }
+
+
+
+
+    navHTML += '<form class="form-inline my-2 my-lg-0">\n' +
+        '           <input class="form-control mr-sm-2" type="search" placeholder="ej: Venom..." aria-label="Search">\n' +
+        '           <button class="btn btn-success my-2 my-sm-0 fa fa-search" type="submit"></button>\n' +
+        '       </form>';
+
+
+    document.getElementById( 'navbarSupportedContent' ).innerHTML = navHTML;
+    buttonNavAction();
+}
+
+
+
+$( document ).ready( function () {
+
+    buildNavegation();
+    loadDefault();
 
 });
 
