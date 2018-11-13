db38eefffb3d07c1c22d6d9fae7dc8c3a1f1c14e
Oliver <oliver.alamo60@gmail.com>
2018-11-13 12:03:24 +0000

    Jason

--- News/News.js
+++ News/News.js
@@ -1,9 +1,11 @@
 
 var News = [
     {
-        name : "Home",
-        id : "link-index",
-        link : "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350"
+        name : "'The Walking Dead': estos son los nuevos personajes a los que debes prestar atención",
+        id : "link-inde",
+        image : "http://localhost:63342/MovieProject/News/imgs/walking.jpg",
+        date : "05/11/2018",
+        description: "La novena temporada de la ficción zombi introducirá gran cantidad de fichajes en su próximo episodio"
     },
     {
         name : "Homee",
@@ -12,16 +14,20 @@ var News = [
     },
 ];
 
-function prueba(){
 
-    var contentHTML ='';
+$( document ).ready(function(){
+   var contentHTML ='';
     for (var i = 0; i < News.length; i++){
-        contentHTML += '<div id="content-prueba">\n' +
-            '           <a  id="' + News[i].id  + News[i].name + News[i].link  +'</a>\n' +
-            '       </div>';
+        contentHTML += '<div class="row shadow p-3 mb-5 bg-white rounded">\n' +
+            '           <img  src="' + News[i].image  +'"</img> '+
+                        '<h1>\n' + News[i].name + '</h1>' + '</div>';
+        //contentHTML += '<div class="row shadow p-3 mb-5 bg-white rounded">\n' '<h1>\n' + News[i].name + '</h1>' + '</div>';
     }
+
+
     console.log(document.getElementById( 'content-prueba' ));
     document.getElementById( 'content-prueba' ).innerHTML = contentHTML;
 
+});
 
-}
+
